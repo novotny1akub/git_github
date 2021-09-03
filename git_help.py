ef83e76cdc2717795e34dcb3cb7eae4d7e9e3874
Jakub Novotny <novotny@mnd.cz>
2021-09-03 17:36:13 +0200

    added push to the description

--- git_help.py
+++ git_help.py
@@ -1,5 +1,18 @@
+# https://naucse.python.cz/course/pyladies/git/basics/
 # git init --> dělám na začátku, abych vytvořil všechny git náležitosti
-# git status
-# git add file_to_add.txt
+# git status --> zjistím status, tedy např. kde jsou "Changes not staged for commit"
+# git add file_to_add.txt --> dělám před commit (můžu i více souborů)
 # git commit
-# git show
+# git show --> ukazuje log předchozího commitu
+# git diff --> změny mezi dvěma commity nebo mezi commitem a stávající verzí repozitáře a commitem
+# git log --> vypíše všechny revize od té nejnovější až po úplný začátek projektu.
+
+
+## shorter
+# First right click the tab corresponding to any file in your repository and click "set console working directory."
+# !git init
+# !git add file_name_goes_here.py
+# !git commit -m "My commit"
+
+# !git remote add origin https://github.com/novotny1akub/git_github.git (zkopírováno z repa na githubu)
+# !git push -u origin master
