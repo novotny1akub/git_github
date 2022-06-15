dcaaf803bdd466decd9c6f4bc8aba322a4c02813
novotny1akub <39886440+novotny1akub@users.noreply.github.com>
2022-06-15 15:04:52 +0200

    Updated instructions

--- git_help.py
+++ git_help.py
@@ -1,18 +1,21 @@
 # https://naucse.python.cz/course/pyladies/git/basics/
-# git init --> dělám na začátku, abych vytvořil všechny git náležitosti
-# git status --> zjistím status, tedy např. kde jsou "Changes not staged for commit"
-# git add file_to_add.txt --> dělám před commit (můžu i více souborů)
-# git commit
-# git show --> ukazuje log předchozího commitu
-# git diff --> změny mezi dvěma commity nebo mezi commitem a stávající verzí repozitáře a commitem
-# git log --> vypíše všechny revize od té nejnovější až po úplný začátek projektu.
 
-
-## shorter
+## short
 # First right click the tab corresponding to any file in your repository and click "set console working directory."
 # !git init
 # !git add file_name_goes_here.py
 # !git commit -m "My commit"
-
 # !git remote add origin https://github.com/novotny1akub/git_github.git (zkopírováno z repa na githubu)
 # !git push -u origin master
+
+## dalsi commandy
+# !git status
+# !git show --> ukazuje log předchozího commitu
+# !git diff --> změny mezi dvěma commity nebo mezi commitem a stávající verzí repozitáře a commitem
+# !git log --> vypíše všechny revize od té nejnovější až po úplný začátek projektu.
+
+## uplne na zacatku
+# !git config --global user.name "Jakub Novotny"
+# !git config --global user.email "my_email_goes_here@example.com"
+# !git config --global --list
+# !git config --global --replace-all user.name "Jakub Novotny"
