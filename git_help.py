42e57f67d433936efae8adfd091ccce187ba437b
novotny1akub <39886440+novotny1akub@users.noreply.github.com>
2024-03-20 15:27:07 +0100

    added more useful git commands
    from a youtube video

--- git_help.py
+++ git_help.py
@@ -24,3 +24,25 @@
 # !git config --global user.email "my_email_goes_here@example.com"
 # !git config --global --list
 # !git config --global --replace-all user.name "Jakub Novotny"
+
+
+### https://www.youtube.com/playlist?list=PLdHg5T0SNpN1dJt6gv9DGSqk5sBjmG3XI
+## making commits
+# git status # if i want to see, for example, what files are not in the staging area
+# git add file_name # to add to the staging area, if you change a file and want to commit it, you need to stage it again!
+# git add . # to add all unstaged files
+# git commit -m "first commit"
+# git log # to see all commits i made, press q to exit
+# git log --oneline
+# git commit --amend -m "changed commit message"
+# git commit --amend -m "New commit message" <commit-id>
+## checkout commits (going back in time)
+# git checkout <commit-hash> # to see how things looked in the past
+## revert commits (undoing things)
+# git revert <commit-hash> # creates a new commit that undoes the changes made by a previous commit
+## merging commits into one - (git rebase and squashing)
+# git rebase -i HEAD~5 # interactively changing posledních 5 commitů, p (beru, jak je), r (beru, ale měním message), s (consolidating more commits into one larger commit.), d (completely remove)
+
+
+
+
